00bcd5f20ed1a14a276732d456574ecd4b353fc3
plaul <lmor@kea.dk>
2022-09-21 18:23:48 +0200

    ready for pull request demo

---
+++ src/test/java/dat3/example/players/repositories/PlayerRepositoryTest.java
@@ -0,0 +1,13 @@
+package dat3.example.players.repositories;
+
+//import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerRepositoryTest {
+
+    //@Test
+    //void fail(){
+       // assertEquals(1,0);
+    //}
+}
